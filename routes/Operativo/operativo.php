c8b10671b82516e0ac788e6a6dcd7fae472626fe
maxhadortiz93 <maxhadortiz93@gmail.com>
2018-05-02 13:15:15 -0400

    0.0.4

---
+++ routes/Operativo/operativo.php
@@ -0,0 +1,8 @@
+Route::group(['prefix'=>'taquilla',],function(){
+		Route::get('/','TaquillaController@taquilla')->name('soloida');
+		Route::get('/idayvuelta','TaquillaController@taquilla')->name('idayvuelta');
+		Route::get('/multidestino','TaquillaController@taquilla')->name('multidestino');
+		
+		Route::get('/DetalleVuelo','TaquillaController@DetalleVuelo')->name('taquilla.DetalleVuelo');
+		
+	});
