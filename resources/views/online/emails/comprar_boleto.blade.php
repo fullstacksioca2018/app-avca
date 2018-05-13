61c1768d6161b9301ff98343211c4f640e20ce7b
HugoZorrilla <hugo_zorrilla@hotmail.com>
2018-05-12 22:39:26 -0400

    antes de email

---
+++ resources/views/online/emails/comprar_boleto.blade.php
@@ -0,0 +1,12 @@
+<P>Estimad@: <strong>Pepito Perez</strong></P>
+<p>En nombre de AVCA, le informamos que su compra fue exitosa</p>
+<p>En la parte de abajo podrá revisar los localizadores para el checking del boleto.</p>
+
+<br>
+<p>Atentamente,</p>
+
+<p>
+    <span>AVCA</span> <br>
+    <span>0293-4332040</span> <br>
+    <span>04167834720</span>
+</p>
