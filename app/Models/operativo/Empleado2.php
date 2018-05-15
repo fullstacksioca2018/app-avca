1bb30dcc979001ee442b727f61a7f052ef5c3e4b
gersvasqz <36513157+gersvasqz@users.noreply.github.com>
2018-05-15 08:24:02 -0400

    Integracion antes de clase gerson

--- app/Models/operativo/Empleado2.php
+++ app/Models/operativo/Empleado2.php
@@ -7,10 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Empleado2 extends Model
 {
     protected $table = "empleados2";
-<<<<<<< HEAD
     protected $primaryKey = "id";
-=======
-    protected $primaryKey = "empleado_id";
->>>>>>> fb0d86a4bd8f3ea53a48ce51b2143b45bd9ac79c
     protected $fillable = ['cedula', 'nombre', 'apellido', 'tipo_empleado',];
 }
